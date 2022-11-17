43b5fadca3a57b2512aa36663b97fe1e3f0df402
German <68060177+lordireality@users.noreply.github.com>
2022-11-17 09:48:27 +0300

    убрал копипаст код с гайдов unity

--- SimpleCarController.cs
+++ SimpleCarController.cs
@@ -4,29 +4,7 @@ using UnityEngine;
 
 public class SimpleCarController : MonoBehaviour
 {
-    //public List<AxleInfo> axleInfos; // the information about each individual axle
-    //public float maxMotorTorque; // maximum torque the motor can apply to wheel
-    //public float maxSteeringAngle; // maximum steer angle the wheel can have
-    //public void FixedUpdate()
-    //{
-    //    float motor = maxMotorTorque * Input.GetAxis("Vertical");
-    //    float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
 
-    //    foreach (AxleInfo axleInfo in axleInfos)
-    //    {
-    //        if (axleInfo.steering)
-    //        {
-    //            axleInfo.leftWheel.steerAngle = steering;
-    //            axleInfo.rightWheel.steerAngle = steering;
-    //        }
-    //        if (axleInfo.motor)
-    //        {
-    //            axleInfo.leftWheel.motorTorque = motor;
-    //            axleInfo.rightWheel.motorTorque = motor;
-    //        }
-    //    }
-    //}
-    //debug speed
     
 
     public WheelCollider frontLeftWheel;
@@ -260,17 +238,6 @@ public class SimpleCarController : MonoBehaviour
         rearRightWheelModel.transform.position = rRWPosition;
     }
 }
-
-//[System.Serializable]
-//public class AxleInfo
-//{
-//    public WheelCollider leftWheel;
-//    public WheelCollider rightWheel;
-//    public bool motor; // is this wheel attached to motor?
-//    public bool steering; // does this wheel apply steer angle?
-//}
-
-
 
 //передачи
 [System.Serializable]
